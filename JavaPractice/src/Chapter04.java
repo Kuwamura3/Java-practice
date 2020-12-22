9cf3e3c10d2bcd2718d7b735ef4f7564bd59de1c
Kuwamura3 <yasu.metallurgy.th@gmail.com>
2020-12-22 18:34:52 +0900

    [Finish] 基礎４章

---
+++ JavaPractice/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name1 = "Kuwamura";
+		System.out.println(name1);
+		name1 = "Yasunori";
+		System.out.println(name1);
+		System.out.println("lucky number");
+		final int NUM1 = 777;
+		System.out.println(NUM1);
+	}
+}

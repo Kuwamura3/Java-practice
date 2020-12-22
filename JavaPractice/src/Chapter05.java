48613bf04b06e1eb58e4dcd410ad79242136ae0f
Kuwamura3 <yasu.metallurgy.th@gmail.com>
2020-12-22 19:30:29 +0900

    [Finish] 基礎５章

---
+++ JavaPractice/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234567890L;
+		System.out.println(long1);
+
+		double double1 = 1.5;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = Float.valueOf(3.14f);
+		System.out.println(float1);
+		
+		Integer integer1 = Integer.valueOf(777);
+		System.out.println(integer1);
+		
+		Character character1 = Character.valueOf('x');
+		System.out.println(character1);
+		
+		int[] array1 = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println(array1[3]);
+	}
+}

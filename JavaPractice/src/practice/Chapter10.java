b9ae43a3e41b1e3acf634f7a05c7448227b2109c
Kuwamura3 <yasu.metallurgy.th@gmail.com>
2020-12-28 19:06:43 +0900

    [Finish] 基礎10章

--- JavaPractice/src/practice/Chapter10.java
+++ JavaPractice/src/practice/Chapter10.java
@@ -13,14 +13,29 @@ public class Chapter10 {
 			System.out.println("validIndexの呼出終了");
 		} catch (IllegalArgumentException e) {
 			System.out.println("IllegalArgumentExceptionが発生しました");
-			e.printStachTrace();
+			e.printStackTrace();
+		}
+		
+		try {
+			Chapter10.throwSQLException();
+		} catch (SQLException s) {
+			System.out.println("SQLExceptionが発生しました");
+			s.printStackTrace();
+		} finally {
+			System.out.println("throwSQLExceptionの呼出終了");
 		}
 		
 		System.out.println("mainメソッド終了");
 	}
 	
-	public static void throwSQLException() throws SQLException {
-		throw new SQLException("SQLエラーです");
+	public static void validIndex(int[] array, int a) {
+		if ( array.length <= a ) {
+			throw new IllegalArgumentException(a + "はサイズの範囲外です");
+		}
+		System.out.println("インデックス" + a + "の要素は" + array[a] + "です");
 	}
 	
+	public static void throwSQLException() throws SQLException {
+		throw new SQLException("SQLエラーです");
+	}	
 }

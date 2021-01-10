fe62f1bde660c44ba2273165bc786051fbcd0845
Kuwamura3 <yasu.metallurgy.th@gmail.com>
2021-01-10 16:42:17 +0900

    [Add] 基礎15章_2　途中保存

---
+++ JavaPractice/src/practice/Chapter15_2.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Chapter15_2 {
+	public static void main(String[] args) {
+		FileSystem fs = FileSystems.getDefault();
+
+		Path inputPath = fs.getPath("resouces/input.txt");
+		Path outputPath = fs.getPath("resouces/output.txt");
+		
+		try () {
+			String line = null;
+			
+			while () {
+				
+			}
+		} catch () {
+			
+		} finally {
+			System.out.println("o—Íˆ—I—¹");
+		}
+	}
+}
